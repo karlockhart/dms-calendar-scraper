8aee662aa00835cf02c657498fa905d48b022e58
Karl Lockhart <Karl Lockhart>
2017-03-14 00:13:28 -0500

    Fixing the scraper.

--- pkg/dms/calendar.go
+++ pkg/dms/calendar.go
@@ -1,10 +1,16 @@
 package dms
 
-import "github.com/PuerkitoBio/goquery"
-import "fmt"
-import "log"
+import (
+	"log"
+	"strings"
+
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 type Event struct {
+	Title   string
 	When    string
 	Where   string
 	Details string
@@ -24,7 +30,22 @@ func NewCalendar() (*Calendar, error) {
 	}
 
 	doc.Find(".event-panel").Each(func(i int, s *goquery.Selection) {
-		fmt.Println(s.Find(".panel-heading").Text())
+		event := new(Event)
+		time := strings.TrimSpace(s.Find(".panel-heading").Find(".time").Text())
+		event.Title = strings.TrimSpace(strings.Replace(s.Find(".panel-heading").Text(), time, "", -1))
+		s.Find(".table-condensed tr").Each(func(i int, tr *goquery.Selection) {
+			label := tr.Find("td:nth-child(1)").Text()
+			switch label {
+			case "Where":
+				event.Where = strings.TrimSpace(tr.Find("td:nth-child(2)").Text())
+			case "When":
+				event.When = strings.TrimSpace(tr.Find("td:nth-child(2)").Text())
+			case "Details":
+				event.Details = strings.TrimSpace(tr.Find("td:nth-child(2)").Text())
+			}
+		})
+		fmt.Println(event.Title, event.When, event.Where, event.Details)
+
 	})
 
 	return nil, err
